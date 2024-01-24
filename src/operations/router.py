6a44cb380d97b3dede2095dc786cf012a8dcf2b5
work <work@gmail.com>
2024-01-24 17:10:01 +0200

    start html

---
+++ src/operations/router.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_async_session
+from operations.models import operation
+from sqlalchemy import select, insert
+from fastapi_cache.decorator import cache
+from operations.schemas import OperationCreate
+import time
+
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operations"]
+)
+
+
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
+
+
+@router.get("/")
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+            "status": "success",
+            "data": result.all(),
+            "details": None
+        }
+    except Exception:
+        # Передать ошибку разработчикам
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
